67f58a9e6433a830ce29163609eb541709339a70
admin <timofeyegorov48@gmail.com>
2021-08-02 20:45:33 +0700

    changes-new

--- test.py
+++ test.py
@@ -1,2 +1,13 @@
-for i in reversed(range(1, 4)):
-    print(i)
+from selenium import webdriver
+import time
+
+driver = webdriver.Chrome(executable_path=r'C:\Users\admin\PycharmProjects\bookmaker-parser\chromedriver.exe')
+url = 'https://selenium-python.readthedocs.io/locating-elements.html#locating-by-id'
+
+try:
+    driver.get(url=url)
+    time.sleep(5)
+except Exception as ex:
+    print(ex)
+finally:
+    driver.quit()
